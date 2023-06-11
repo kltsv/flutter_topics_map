1e2b1372cfe708c8313d6bb26b63f26fe8fd3acc
Sergey Koltsov <srg.koltsov@yandex.ru>
2023-06-11 08:12:41 +0300

    Clip the first and the last tile in a list that is placed inside a card widget

--- lib/topics_list_view.dart
+++ lib/topics_list_view.dart
@@ -16,63 +16,95 @@ class TopicsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        final description = topicDescription;
-        if (description != null && index == 0) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(description),
-          );
-        }
-        return TopicTile(
-          topicNode: topics[index - (description != null ? 1 : 0)],
-        );
-      },
-      itemCount: topics.length + (topicDescription != null ? 1 : 0),
+    final descriptionOffset = topicDescription != null ? 1 : 0;
+    final hasDescription = topicDescription != null;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            final description = topicDescription;
+            if (description != null && index == 0) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(description),
+              );
+            }
+            final clipRadius = hasDescription
+                ? null
+                : index == 0
+                    ? const BorderRadius.vertical(top: Radius.circular(12))
+                    : index == (topics.length - 1)
+                        ? const BorderRadius.vertical(
+                            bottom: Radius.circular(12))
+                        : null;
+
+            final tile = Dismissible(
+              key: ValueKey(
+                topics[index - descriptionOffset].topic,
+              ),
+              background: Container(
+                decoration: BoxDecoration(
+                  borderRadius: clipRadius,
+                  color: Colors.red,
+                ),
+              ),
+              child: TopicTile(
+                topicNode: topics[index - descriptionOffset],
+                borderRadius: clipRadius,
+              ),
+            );
+
+            return tile;
+          },
+          itemCount: topics.length + descriptionOffset,
+        ),
+      ),
     );
   }
 }
 
 class TopicTile extends StatelessWidget {
   final TopicNode topicNode;
+  final BorderRadiusGeometry? borderRadius;
 
   const TopicTile({
     Key? key,
     required this.topicNode,
+    this.borderRadius,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 14),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        title: HeroTopicTitle(topicNode: topicNode),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(topicNode.description),
-            Wrap(
-              children: topicNode.subtopics
-                  .map(
-                    (subtopic) => TopicChip(topic: subtopic),
-                  )
-                  .toList(),
-            ),
-          ],
-        ),
-        onTap: topicNode.subtopics.isNotEmpty
-            ? () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TopicPage(topic: topicNode),
-                  ),
+    final radius = borderRadius;
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 14),
+      title: HeroTopicTitle(topicNode: topicNode),
+      shape:
+          radius != null ? RoundedRectangleBorder(borderRadius: radius) : null,
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(topicNode.description),
+          Wrap(
+            children: topicNode.subtopics
+                .map(
+                  (subtopic) => TopicChip(topic: subtopic),
                 )
-            : null,
+                .toList(),
+          ),
+        ],
       ),
+      onTap: topicNode.subtopics.isNotEmpty
+          ? () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TopicPage(topic: topicNode),
+                ),
+              )
+          : null,
     );
   }
 }
